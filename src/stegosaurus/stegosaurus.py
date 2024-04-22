0d04e941ddfbf48f55934338eeca635cd2edf2ed
Rosana <117678628+rosananana@users.noreply.github.com>
2024-04-22 17:52:36 -0400

    Update stegosaurus.py
    decrypt doesn't work but in progress of using delimiter

--- src/stegosaurus/stegosaurus.py
+++ src/stegosaurus/stegosaurus.py
@@ -19,15 +19,13 @@ def encode(img, message, name):
     elif len(color_array[1]) == 4:
         n = 4
 
-    # Translating message into binary
-    # message += b'' # Delimiter
-    print("\n", message)
+    message += b'$3cur17y' # delimeter so we know where message ends
+    # print(f"Message w/ delimiter: {message}")
     message = [str(access_bit(message, i)) for i in range(len(message)*8)]
     message_binary = "".join(message)
     total_pixels = len(message_binary)
     print(f"Total Pixels: {total_pixels} \nBinary Stream: {message_binary}\n")
 
-    # Modifying LSB values
     pixels_amount = len(color_array)
     if total_pixels > pixels_amount:
         print("Image is too small to encode")
@@ -45,42 +43,40 @@ def encode(img, message, name):
                     # print(bin(color_array[pix][rgb]))
                     # print(f"After: {color_array[pix][rgb]}\n")
                     i += 1
-        print(color_array)
+
+        # print(color_array)
         color_array = color_array.reshape(height, width, n)
         encoded_img = Image.fromarray(color_array.astype("uint8"), cover.mode)
         encoded_img.save(f"{name}")
-        print("Image encoded")
-    return i
+        print("End: Stego Object Image Created\n")
+        # return i
 
-def decode(stegoimg, L):
+def decode(stegoimg):
     # Getting image's [R, G, B] values
     stego = Image.open(stegoimg, "r")
-    width, height = stego.size
     color_array = np.array(list(stego.getdata()))
     if len(color_array[0]) == 3:
         n = 3
     elif len(color_array[1]) == 4:
         n = 4
 
-    doesthiswork = ""
-    # Modifying LSB values
+    # retrieve message bits
+    retrieve_bits = ""
     pixels_amount = len(color_array)
-    i = 0       # Index counter for message
     for pix in range(pixels_amount):
-        for rgb in range(n):     # Bit of R, G, and B
-            if i < L:    # As long as enough pixels
-                doesthiswork += bin(color_array[pix][rgb])[-1]
-                i += 1
-    print("BinaryStream2:", doesthiswork)
-        
-    # Translating message into binary
-    """message = [str(access_bit(message, i)) for i in range(len(message)*8)]
-    message_binary = "".join(message)
-    # print(message_binary)
-    total_pixels = len(message_binary)
-    # image_rrg.png encoded.jpg
-    print(f"Total Pixels: {total_pixels} \nBinary Stream: {message_binary}\n")"""
+        for rgb in range(n): # Bit of R, G, and B
+            retrieve_bits += bin(color_array[pix][rgb])[-1]
+    print("DECRYPT - BinaryStream:", retrieve_bits)
 
+    delimiter = b'$3cur17y'
+    delimiter_to_bits = bin(int.from_bytes(delimiter, byteorder = "big"))[2:]
+    print(f"Delimiter in Binary: {delimiter_to_bits}\n")
+    temp = retrieve_bits.partition(delimiter_to_bits)
+    print(temp)
+    return retrieve_bits
+"""
+
+"""
 
 def AES_encrypt(data, key):
     cipher = AES.new(key, AES.MODE_EAX)
@@ -96,20 +92,42 @@ def AES_decrypt(ciphertext, key, nonce, tag):
     
 
 def main():
-    # AES Encryption
-    data = b'this is a test'
-    key = os.urandom(16)
-    c, n, t = AES_encrypt(data, key)
 
-    print("If RGB --> .jpg, If RGBA --> .png")
-    print("input: <cover> <out_name.jpg or out_name.png>")
-    # 10x10.jpg data encoded.jpg
-    cover, name = input().split()
-    L = encode(cover, c, name)
-    decode("encrypted.png", L)
-    #print("CIPHERTEXT: ", c)
-    # pt = AES_decrypt(c, key, n, t)
-    #print("DECRYPTED PLAINTEXT ", pt)
+    # creating stego object
+    print("Info: cover can be .jpg/.png etc, out file is in .png")
+    print("Enter Input: <cover> <data file.txt> <out_name.png>")
+    # 10x10.jpg data.txt test.png
+
+    # AES Encryption
+    cover, data, out_name = input().split()
+    f = open(data, "r")
+    text = f.read()
+    f.close()
+    data_bytes = str.encode(text)
+    key = os.urandom(16)
+    cipher, nonce, tag = AES_encrypt(data_bytes, key)
+
+    print(f"Original Text: {text}")
+
+    encode(cover, cipher, out_name)
+
+    print("Do you want to decode? Yes or No")
+    decoding = input()
+    if decoding == "Yes":
+        # deciphering given image
+        print("Info: give stego object and output txt")
+        print("Enter Input: <stego_object.png>")
+        stego = input()
+        original = decode(stego)
+        print(f"original: {original}")
+        plaintext = AES_decrypt(original, key, nonce, tag)
+        print(f"DECRYPTED PLAINTEXT: {plaintext}")
+    else:
+        pass
+    # check
+    # print("CIPHERTEXT: ", cipher)
+    # pt = AES_decrypt(cipher, key, nonce, tag)
+    # print("DECRYPTED PLAINTEXT ", pt)
 
 if __name__ == "__main__":
     main()
