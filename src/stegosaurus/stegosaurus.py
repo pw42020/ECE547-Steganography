11b4526955c2b0f047c29cb78c65ad473aafef60
EvanRaftery <eraftery@umass.edu>
2024-04-22 12:38:43 -0400

    Primitive Decryption
    Added a basic version of the decode method to retrieve our password from the image (needs work/improvement).

--- src/stegosaurus/stegosaurus.py
+++ src/stegosaurus/stegosaurus.py
@@ -10,7 +10,6 @@ def access_bit(data, num):
 
 
 def encode(img, message, name):
-
     # Getting image's [R, G, B] values
     cover = Image.open(img, "r")
     width, height = cover.size
@@ -20,15 +19,15 @@ def encode(img, message, name):
     elif len(color_array[1]) == 4:
         n = 4
 
+    # Translating message into binary
     # message += b'' # Delimiter
-    # print(message)
+    print("\n", message)
     message = [str(access_bit(message, i)) for i in range(len(message)*8)]
     message_binary = "".join(message)
-    # print(message_binary)
     total_pixels = len(message_binary)
-    # image_rrg.png encoded.jpg
     print(f"Total Pixels: {total_pixels} \nBinary Stream: {message_binary}\n")
 
+    # Modifying LSB values
     pixels_amount = len(color_array)
     if total_pixels > pixels_amount:
         print("Image is too small to encode")
@@ -46,12 +45,41 @@ def encode(img, message, name):
                     # print(bin(color_array[pix][rgb]))
                     # print(f"After: {color_array[pix][rgb]}\n")
                     i += 1
-
         print(color_array)
         color_array = color_array.reshape(height, width, n)
         encoded_img = Image.fromarray(color_array.astype("uint8"), cover.mode)
         encoded_img.save(f"{name}")
         print("Image encoded")
+    return i
+
+def decode(stegoimg, L):
+    # Getting image's [R, G, B] values
+    stego = Image.open(stegoimg, "r")
+    width, height = stego.size
+    color_array = np.array(list(stego.getdata()))
+    if len(color_array[0]) == 3:
+        n = 3
+    elif len(color_array[1]) == 4:
+        n = 4
+
+    doesthiswork = ""
+    # Modifying LSB values
+    pixels_amount = len(color_array)
+    i = 0       # Index counter for message
+    for pix in range(pixels_amount):
+        for rgb in range(n):     # Bit of R, G, and B
+            if i < L:    # As long as enough pixels
+                doesthiswork += bin(color_array[pix][rgb])[-1]
+                i += 1
+    print("BinaryStream2:", doesthiswork)
+        
+    # Translating message into binary
+    """message = [str(access_bit(message, i)) for i in range(len(message)*8)]
+    message_binary = "".join(message)
+    # print(message_binary)
+    total_pixels = len(message_binary)
+    # image_rrg.png encoded.jpg
+    print(f"Total Pixels: {total_pixels} \nBinary Stream: {message_binary}\n")"""
 
 
 def AES_encrypt(data, key):
@@ -69,7 +97,7 @@ def AES_decrypt(ciphertext, key, nonce, tag):
 
 def main():
     # AES Encryption
-    data = b'this is a test.'
+    data = b'this is a test'
     key = os.urandom(16)
     c, n, t = AES_encrypt(data, key)
 
@@ -77,8 +105,8 @@ def main():
     print("input: <cover> <out_name.jpg or out_name.png>")
     # 10x10.jpg data encoded.jpg
     cover, name = input().split()
-    encode(cover, c, name)
-    
+    L = encode(cover, c, name)
+    decode("encrypted.png", L)
     #print("CIPHERTEXT: ", c)
     # pt = AES_decrypt(c, key, n, t)
     #print("DECRYPTED PLAINTEXT ", pt)
